cee44a8432ba0f122ad622f9ecb7dc01dc261712
boncho valkov <bonchovylkov@abv.bg>
2013-08-24 21:32:50 +0300

    battle game client

---
+++ Scripts/controller.js
@@ -0,0 +1,335 @@
+﻿/// <reference path="class.js" />
+/// <reference path="persister.js" />
+/// <reference path="jquery-2.0.2.js" />
+/// <reference path="ui.js" />
+
+var controllers = (function () {
+    var rootUrl = "http://localhost:22954/api/";
+	var Controller = Class.create({
+		init: function () {
+			this.persister = persisters.get(rootUrl);
+		},
+		loadUI: function (selector) {
+			if (this.persister.isUserLoggedIn()) {
+				this.loadGameUI(selector);
+			}
+			else {
+				this.loadLoginFormUI(selector);
+			}
+			this.attachUIEventHandlers(selector);
+		},
+		loadLoginFormUI: function (selector) {
+			var loginFormHtml = ui.loginForm()
+			$(selector).html(loginFormHtml);
+		},
+		loadGameUI: function (selector) {
+		    var self = this;
+			var list =
+				ui.gameUI(this.persister.nickname());
+			$(selector).html(list);
+
+			this.updateUI(selector);
+
+		    updateTimer = setInterval(function () {
+		    	self.updateUI(selector);
+		    }, 15000);
+
+			
+		},
+		loadGame: function (selector, gameId) {
+		    var self = this;
+		    this.persister.game.field(gameId, function (gameState) {
+		        //$(selector).append(JSON.stringify(gameState));
+				var gameHtml = ui.gameField(gameState);
+				$(selector + " #game-holder").html(gameHtml);
+				var fieldCells = $("td");
+				for (var i = 0; i < fieldCells.length; i++) {
+				   // $("#wrapper").append($(fieldCells[i]).text());
+				    if ($(fieldCells[i]).text().toString().slice(0,1) == "W") {
+				        $(fieldCells[i]).attr({ "attack": "8", "armor": "3", "hitPoints": "15", "range": "1", "speed": "2" });
+				        
+				        //alert("W");
+				    }
+				    else if ($(fieldCells[i]).text().toString().slice(0, 1) == "R") {
+				        $(fieldCells[i]).attr({ "attack": "8", "armor": "1", "hitPoints": "10", "range": "3", "speed": "1" });
+				        //alert("R");
+				    }
+				}
+
+
+				var inTurn = gameState.inTurn;
+				var playerInTurn = gameState[inTurn].nickname;
+				ui.appendMakeGuessField(playerInTurn,inTurn, gameState, self.persister.nickname());
+
+
+			});
+		},
+		attachUIEventHandlers: function (selector) {
+			var wrapper = $(selector);
+			var self = this;
+
+			wrapper.on("click", "#btn-show-login", function () {
+				wrapper.find(".button.selected").removeClass("selected");
+				$(this).addClass("selected");
+				wrapper.find("#login-form").show(2000);
+				wrapper.find("#register-form").hide(2000);
+			});
+			wrapper.on("click", "#btn-show-register", function () {
+				wrapper.find(".button.selected").removeClass("selected");
+				$(this).addClass("selected");
+				wrapper.find("#register-form").show(2000);
+				wrapper.find("#login-form").hide(2000);
+			});
+
+			wrapper.on("click", "#btn-login", function () {
+				var user = {
+				    username: $("<div />").html($(selector + " #tb-login-username").val()).text(),
+				    password: $("<div />").html($(selector + " #tb-login-password").val()).text()
+				}
+
+				self.persister.user.login(user, function () {
+					self.loadGameUI(selector);
+				}, function (err) {
+				    alert(err.responseJSON.Message);
+				    //wrapper.html("loign eror.." + JSON.stringify(err));
+				});
+				return false;
+			});
+
+		    //$("<div />").html(game.title).text() 
+			wrapper.on("click", "#btn-register", function () {
+			    var user = {
+			        username: $("<div />").html($(selector + " #tb-register-username").val()).text(),
+			        nickname: $("<div />").html($(selector + " #tb-register-username").val()).text(),
+			        password: $("<div />").html($(selector + " #tb-register-password").val()).text()
+			    };
+			    self.persister.user.register(user, function () {
+			        self.loadGameUI(selector);
+			    }, function (err) {
+			        alert(err.responseJSON.Message);
+			        //wrapper.html("register eror.." + JSON.stringify(err));
+			    });
+			    return false;
+			});
+			wrapper.on("click", "#btn-logout", function () {
+				self.persister.user.logout(function () {
+					self.loadLoginFormUI(selector);
+				});
+			});
+
+			wrapper.on("click", "#open-games-container a", function () {
+				$("#game-join-inputs").remove();
+				var html =
+					'<div id="game-join-inputs">' +
+						'Password: <input type="text" id="tb-game-pass"/>' +
+						'<button id="btn-join-game">join</button>' +
+					'</div>';
+				$(this).after(html);
+			});
+			wrapper.on("click", "#btn-join-game", function () {
+				var game = {
+					id: $(this).parents("li").first().data("game-id")
+				};
+
+				var password = $("#tb-game-pass").val();
+
+				if (password) {
+					game.password = password;
+				}
+				self.persister.game.join(game, function (data) {
+				    alert("join OK" + JSON.stringify(data));
+				}, function (err) {
+				    alert(JSON.stringify(err))
+				});
+			});
+			wrapper.on("click", "#btn-create-game", function () {
+				var game = {
+					title: $("#tb-create-title").val(),
+				}
+				var password = $("#tb-create-pass").val();
+				if (password) {
+					game.password = password;
+				}
+				self.persister.game.create(game);
+			});
+
+			wrapper.on("click", ".active-games .in-progress", function () {
+				self.loadGame(selector, $(this).parent().data("game-id"));
+			});
+			wrapper.on("click", ".btn-start-game", function () {
+			    var gameID = $(this).parent().attr("data-game-id");
+			    
+			    self.persister.game.start(gameID, function () {
+			        alert("game started");
+			    }, function () {
+			        alert("fail game started");
+			    });
+			});
+
+            //TODOO
+			wrapper.on("click", "#btn-move", function () {
+
+			    var gameId = $("#game-state").attr("data-game-id");
+			    var destinationCords = $(this).prev("input#destination").val();
+			    var selectedCell = $("#selected-cell").val(); // $("#" + cell);
+			    var td = $("#" + selectedCell);
+			  
+
+			    var moveParams = {
+			        unitId: td.text().slice(1),
+			        position: { "x": destinationCords.slice(0, 1), "y": destinationCords.slice(1, 2) }
+
+			    };
+			    
+			    self.persister.battle.move(moveParams, gameId, function (data) {
+	
+			        for (var i = 0; i < 9; i++) {
+			            for (var j = 0; j < 9; j++) {
+			                if (selectedCell==i+j) {
+			                    $("#" + i + j).html("");
+			                }
+			            }
+			        }
+			        self.loadGame("#wrapper", gameId);
+			        
+			        alert("unit moved" + JSON.stringify(data));
+
+			    }, function (err) {
+			        console.log(JSON.stringify(err));
+			        alert(err.responseJSON.Message);
+			    })
+			});
+
+			wrapper.on("click", "#btn-attack", function () {
+
+			    var gameId = $("#game-state").attr("data-game-id");
+			    var destinationCords = $("input#destination").val();
+			    var selectedCell = $("#selected-cell").val(); // $("#" + cell);
+			    var td = $("#" + selectedCell);
+
+
+			    var moveParams = {
+			        unitId: td.text().slice(1),
+			        position: { "x": destinationCords.slice(0, 1), "y": destinationCords.slice(1, 2) }
+
+			    };
+
+			    self.persister.battle.attack(moveParams, gameId, function (data) {
+
+			       
+			        for (var i = 0; i < 9; i++) {
+			            for (var j = 0; j < 9; j++) {
+			                if (selectedCell == i + j) {
+			                    $("#" + i + j).html("");
+			                }
+			            }
+			        }
+			        self.loadGame("#wrapper", gameId);
+			        alert("unit attacked at " + moveParams.position.x + moveParams.position.y);
+
+			    }, function (err) {
+			        console.log(JSON.stringify(err));
+			        alert(err.responseJSON.Message);
+			    })
+			});
+
+			wrapper.on("click", "#btn-defend", function () {
+
+			    var gameId = $("#game-state").attr("data-game-id");
+			    var destinationCords = $("#destination").val();
+			    var selectedCell = $("#selected-cell").val(); // $("#" + cell);
+			    var td = $("#" + selectedCell);
+
+			    var defendedUnit = td.text().slice(1);
+
+
+			    self.persister.battle.move(defendedUnit, gameId, function (data) {
+
+			        self.loadGame("#wrapper", gameId);
+			        alert("unit defended " + defendedUnit);
+
+			    }, function (err) {
+			        console.log(JSON.stringify(err));
+			        alert(err.responseJSON.Message);
+			    })
+			});
+
+			wrapper.on("click", "#show-messages", function () {
+			    var next = $(this).next("div");
+			    if (next.hasClass("hidden")) {
+			        next.removeClass("hidden");
+			        next.addClass("expand");
+			        next.show(2000);
+			    }
+			    else {
+			        next.removeClass("expand");
+			        next.addClass("hidden");
+			        next.hide(2000);
+			    }
+			    
+			});
+		},
+
+		updateUI: function (selector) {
+
+	        this.persister.game.open(function (games) {
+	            var list = ui.openGamesList(games);
+	            $(selector + " #open-games")
+					.html(list);
+	        });
+
+
+	        var self = this;
+	        this.persister.game.myActive(function (games) {
+	            var list = ui.activeGamesList(games);
+	            $(selector + " #active-games")
+					.html(list);
+	            //alert(JSON.stringify(games));
+	            ui.appendStartGameButton(self.persister.nickname());
+	        });
+
+	        this.persister.massage.all(function (msgs) {
+	            //apend then throu ui
+	            ui.buildAllMsgs(msgs);
+
+	        }, function (err) {
+	           // alert(err.responseJSON.Message);
+	        });
+
+	        this.persister.massage.unread(function (msgs) {
+	            //apend then throu ui
+	            ui.buildUnreadMsgs(msgs);
+	            //alert("loaded unread msg" + JSON.stringify(msgs));
+	        }, function (err) {
+	           // alert(err.responseJSON.Message);
+	        });
+
+	        this.persister.user.scores(function (scores) {
+	            var currentNickname = self.persister.nickname();
+	            var currentScore = "";
+	            for (var i = 0; i < scores.length; i++) {
+	                if (scores[i].nickname == currentNickname) {
+	                    currentScore = scores[i].score;
+	                }
+	            }
+	            $("#player-score").html("  Player Score : "+currentScore);
+	            //alert(JSON.stringify(scores));
+	            ui.appendScoreBoard(scores);
+	        }, function (err) {
+	            //alert(err.responseJSON.Message);
+	        });
+    }
+
+	});
+	return {
+		get: function () {
+			return new Controller();
+		}
+	}
+}());
+
+$(function () {
+	var controller = controllers.get();
+	controller.loadUI("#content");
+	
+});
---
+++ Scripts/ui.js
@@ -0,0 +1,261 @@
+﻿/// <reference path="jquery-2.0.2.js" />
+
+
+var ui = (function () {
+
+    function buildLoginForm() {
+        var html =
+            '<div id="login-form-holder">' +
+				'<form>' +
+					'<div id="login-form">' +
+						'<label for="tb-login-username">Username: </label>' +
+						'<input type="text" id="tb-login-username"><br />' +
+						'<label for="tb-login-password">Password: </label>' +
+						'<input type="password" id="tb-login-password"><br />' +
+						'<button id="btn-login" class="button">Login</button>' +
+					'</div>' +
+					'<div id="register-form" style="display: none">' +
+						'<label for="tb-register-username">Username: </label>' +
+						'<input type="text" id="tb-register-username"><br />' +
+						'<label for="tb-register-nickname">Nickname: </label>' +
+						'<input type="text" id="tb-register-nickname"><br />' +
+						'<label for="tb-register-password">Password: </label>' +
+						'<input type="password" id="tb-register-password"><br />' +
+						'<button id="btn-register" class="button">Register</button>' +
+					'</div>' +
+					'<a href="#" id="btn-show-login" class="button selected">Login</a>' +
+					'<a href="#" id="btn-show-register" class="button">Register</a>' +
+				'</form>' +
+            '</div>';
+        return html;
+    }
+
+    function buildGameUI(nickname) {
+        var html = '<span id="user-nickname">' +
+				$("<div />").html(nickname).text() +
+		'</span>' +
+       "<span id='player-score'></span>" +
+		'<button id="btn-logout">Logout</button><br/>' +
+		'<div id="create-game-holder">' +
+			'Title: <input type="text" id="tb-create-title" />' +
+			'Password: <input type="text" id="tb-create-pass" />' +
+			'<button id="btn-create-game">Create</button>' +
+		'</div>' +
+		'<div id="open-games-container">' +
+			'<h2>Open</h2>' +
+			'<div id="open-games"></div>' +
+		'</div>' +
+		'<div id="active-games-container">' +
+			'<h2>Active</h2>' +
+			'<div id="active-games"></div>' +
+		'</div>' +
+		'<div id="game-holder">' +
+		'</div>' +
+		'<div id="all-msg-container">' +
+			'<h2 id="show-messages">Click to see All Messages!!</h2>' +
+			'<div id="all-msgs"></div>' +
+		'</div>' +
+		'<div id="unread-msg-container">' +
+			'<h2>Unread Messages</h2>' +
+			'<div id="unread-msgs"></div>' +
+		'</div>';
+        return html;
+    }
+
+    function buildOpenGamesList(games) {
+        var list = '<ul class="game-list open-games">';
+        for (var i = 0; i < games.length; i++) {
+            var game = games[i];
+            list +=
+				'<li data-game-id="' + game.id + '">' +
+					'<a href="#" >' +
+						$("<div />").html(game.title).text() +
+					'</a>' +
+					'<span> by ' +
+						game.creator +
+					'</span>' +
+				'</li>';
+        }
+        list += "</ul>";
+        return list;
+    }
+
+    function buildActiveGamesList(games) {
+        var gamesList = Array.prototype.slice.call(games, 0);
+        gamesList.sort(function (g1, g2) {
+            if (g1.status == g2.status) {
+                return g1.title > g2.title;
+            }
+            else {
+                if (g1.status == "in-progress") {
+                    return -1;
+                }
+            }
+            return 1;
+        });
+
+        var list = '<ul class="game-list active-games">';
+        for (var i = 0; i < gamesList.length; i++) {
+            var game = gamesList[i];
+            list +=
+				'<li data-game-id="' + game.id + '">' +
+					'<a href="#" class="' + game.status + '">' +
+						$("<div />").html(game.title).text() +
+					'</a>' +
+					'<span  data-creator="' + game.creator + '"> by ' +
+						game.creator +
+					'</span>' +
+				'</li>';
+
+
+        }
+        list += "</ul>";
+
+        return list;
+    }
+
+
+
+    function buildTheField(blueUnits, redUnits) {
+        var FIELD_SIZE = 9;
+
+        
+
+        var tableHtml = '<table id="field">';
+        for (var i = 0; i < FIELD_SIZE; i++) {
+            tableHtml += "<tr>";
+            for (var j = 0; j < FIELD_SIZE; j++) {
+                tableHtml += "<td id='" + i + j + "'>";
+                for (var pl = 0; pl < FIELD_SIZE; pl++) {
+                    var bluePlayer = blueUnits[pl];
+                    var redPlayer = redUnits[pl];
+
+                    if (bluePlayer.position.x == i && bluePlayer.position.y == j) {
+                        haveFound = true;
+                        if (bluePlayer.type == "warrior") {
+                            tableHtml += "W<img src='../imgs/wor.jpg' />" + bluePlayer.id + "";
+                        }
+                        else if (bluePlayer.type == "ranger") {
+                            tableHtml += "R<img src='../imgs/ren.jpg' />" + bluePlayer.id + "";
+                        }
+                    }
+                    else if (redPlayer.position.x == i && redPlayer.position.y == j) {
+                        haveFound = true;
+                        if (bluePlayer.type == "warrior") {
+                            tableHtml += "W<img src='../imgs/wor.jpg' />" + redPlayer.id + "";
+                        }
+                        else if (bluePlayer.type == "ranger") {
+                            tableHtml += "R<img src='../imgs/ren.jpg' />" + redPlayer.id + "";
+                        }
+                    }
+
+                }
+                tableHtml += "</td>";
+            }
+            tableHtml += "</tr>";
+        }
+        tableHtml += '</table>';
+
+        return tableHtml; //+ JSON.stringify(blueUnits) + "-------------" + JSON.stringify(redUnits);
+
+        // return JSON.stringify(blueUnits) + "-------------" + JSON.stringify(redUnits);
+    }
+
+
+    function gameField(gameState) {
+        var html =
+			'<div id="game-state" data-game-id="' + gameState.gameId + '">' +
+				'<h2>' + gameState.title + '</h2>' +
+                '<h2> Turn to user: ' + gameState.inTurn + '</h2>' +
+				'<div id="blue-player" class="guess-holder">' +
+					'<h3>' +
+						gameState.blue.nickname + ' -blue player' +
+					'</h3>' +
+				'</div>' +
+                "<div >" +
+                    buildTheField(gameState.blue.units, gameState.red.units) +
+                "</div>" +
+				'<div id="red-player" class="guess-holder">' +
+					'<h3>' +
+						gameState.red.nickname + ' - red -player' +
+					'</h3>' +
+				'</div>' +
+		'</div>';
+        return html;
+    }
+
+    function appendMakeGuessField(inTurn, inTurnColor, gameState, nickname) {
+        if (inTurn == nickname) {
+            $("#" + inTurnColor + "-player").append("Select cell with player(00..88)<input type='text' id='selected-cell'/> to cell" +
+                                                        "<input type='text' id='destination'/>" +
+                                                        "<button id='btn-move'>Move</button>" + 
+                                                        "<button id='btn-attack'>Atack</button>" +
+                                                        "<button id='btn-attack'>Defend</button>" );
+        }
+        else {
+            $("#" + inTurnColor + "-player").append("It is other player move");
+        }
+
+    }
+
+    function appendStartGameButton(nickname) {
+        var button = "<button class='btn-start-game'>Start</button>"
+        var fullGames = $(".full");
+
+        for (var i = 0; i < fullGames.length; i++) {
+            var currentFullGame = $(fullGames[i]);
+            if (currentFullGame.next("span").attr("data-creator") == $("<div />").html(nickname).text()) {
+                currentFullGame.after(button);
+            }
+        }
+
+    }
+
+    function buildAllMsgs(msgs) {
+
+        var allMsgs = "";
+        for (var i = 0; i < msgs.length; i++) {
+            allMsgs += "<span>Massage " + (i + 1) + ": " + msgs[i].text + "! With id: " + msgs[i].gameId + "</span></br>";
+        }
+        $("#all-msgs").html(allMsgs);
+        $("#all-msgs").addClass("hidden");
+        $("#all-msgs").hide(2000);
+    }
+
+    function buildUnreadMsgs(msgs) {
+
+        var allMsgs = "";
+        for (var i = 0; i < msgs.length; i++) {
+            allMsgs += "<span>Massage " + (i + 1) + ": " + msgs[i].text + "! With id: " + msgs[i].gameId + "</span></br>";
+        }
+        $("#unread-msgs").html(allMsgs);
+    }
+
+    function appendScoreBoard(scores) {
+        scores.sort(function (a, b) { return parseInt(b.score) - parseInt(a.score) });
+
+        $("#wrapper").append("<div id='score-board'></div>");
+        var container = "<h1>Wall of fame</h1>";
+        container += "<ul>";
+        for (var i = 0; i < scores.length; i++) {
+            container += "<li><strong> " + scores[i].nickname + "</strong> with scores <strong>" + scores[i].score + "</strong></li>"
+        }
+        container += "</ul>";
+        $("#score-board").html(container);
+
+    }
+
+    return {
+        gameUI: buildGameUI,
+        openGamesList: buildOpenGamesList,
+        loginForm: buildLoginForm,
+        activeGamesList: buildActiveGamesList,
+        gameField: gameField,
+        appendStartGameButton: appendStartGameButton,
+        appendMakeGuessField: appendMakeGuessField,
+        buildAllMsgs: buildAllMsgs,
+        buildUnreadMsgs: buildUnreadMsgs,
+        appendScoreBoard: appendScoreBoard
+    }
+
+}());
